db9e22d5aa6fb8c2bd75151484d1305429da6819
visheratin <alexvish91@gmail.com>
2020-02-15 22:47:56 +0300

    Added comments.

--- data/block.go
+++ data/block.go
@@ -1,5 +1,6 @@
 package data
 
+// Block is an index basic data structure that stores metainformation about the data part in the storage.
 type Block struct {
 	Size  int
 	ElNum int
--- data/element.go
+++ data/element.go
@@ -1,5 +1,6 @@
 package data
 
+// Element describes time-value pairs that are stored in the database.
 type Element struct {
 	Timestamp int64   `parquet:"name=ts, type=INT64"`
 	Value     float64 `parquet:"name=v, type=DOUBLE"`
