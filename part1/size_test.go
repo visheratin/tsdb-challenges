81844b0a1fba99390c802a80aa58783237a1e5dc
visheratin <alexvish91@gmail.com>
2020-02-15 22:49:33 +0300

    Fixed benchmark after refactoring.

--- part1/size_test.go
+++ part1/size_test.go
@@ -9,22 +9,34 @@ import (
 func TestIndexSize(t *testing.T) {
 	var m1, m2, m3, m4, m5, m6, m7, m8 runtime.MemStats
 	runtime.ReadMemStats(&m1)
-	idx := createIndex("slice", 1000000)
+	idx, err := CreateIndex("slice", 1000000)
+	if err == nil {
+		t.Error(err)
+	}
 	runtime.ReadMemStats(&m2)
 	sSize := m2.TotalAlloc - m1.TotalAlloc
 	t.Logf("%d bytes - slice index size\n", sSize)
 	runtime.ReadMemStats(&m3)
-	idx = createIndex("tree", 1000000)
+	idx, err = CreateIndex("tree", 1000000)
+	if err == nil {
+		t.Error(err)
+	}
 	runtime.ReadMemStats(&m4)
 	tSize := m4.TotalAlloc - m3.TotalAlloc
 	t.Logf("%d bytes - tree index size\n", tSize)
 	runtime.ReadMemStats(&m5)
-	idx = createIndex("advTree", 1000000)
+	idx, err = CreateIndex("advTree", 1000000)
+	if err == nil {
+		t.Error(err)
+	}
 	runtime.ReadMemStats(&m6)
 	atSize := m6.TotalAlloc - m5.TotalAlloc
 	t.Logf("%d bytes - advanced tree index size\n", atSize)
 	runtime.ReadMemStats(&m7)
-	idx = createIndex("bTree", 1000000)
+	idx, err = CreateIndex("bTree", 1000000)
+	if err == nil {
+		t.Error(err)
+	}
 	runtime.ReadMemStats(&m8)
 	btSize := m8.TotalAlloc - m7.TotalAlloc
 	t.Logf("%d bytes - b-tree index size\n", btSize)
